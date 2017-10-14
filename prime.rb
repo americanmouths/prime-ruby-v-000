52d4ab34f5027f6118f2fb5eb88a840defd8508f
Kayla Hansen <shiverr@gmail.com>
2017-10-14 01:27:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(number)
   (2..number-1).each do |num|
-    return false if number % num == 0 || number.negative?
+    return false if number % num == 0 || num.negative?
 end
   true
 end
