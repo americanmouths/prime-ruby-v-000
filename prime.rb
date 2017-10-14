ed5d450c7da4973efd9fdb6898a623cde481e3df
Kayla Hansen <shiverr@gmail.com>
2017-10-14 01:14:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-  if numbers.any?{|i| number % i == 0 || number % i == i}
+  if number.any?{|i| number % i == 0 || number % i == i}
     true
   else
     false
