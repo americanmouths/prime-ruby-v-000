87e5c31eefeca9cd0a560f315cca0104bac38eb7
Kayla Hansen <shiverr@gmail.com>
2017-10-14 01:05:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
   return false if number < 2
   return true if number == 3 || number == 2
   if (2..number-1).any?{|i| number % 1 == 0}
-    false
-  else
     true
+  else
+    false
 end
 end
