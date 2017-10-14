80e9de254f8238b3435c8424f0e24cb730430ffb
Kayla Hansen <shiverr@gmail.com>
2017-10-14 01:08:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
     false
   elsif number == 3 || number == 2
     true
-  elsif (2..number-1).any?{|i| number % 1 == 0}
+  elsif (2..number-1).any?{|i| number % 1 == 0 || number % 0 == 0}
     true
   else
     false
