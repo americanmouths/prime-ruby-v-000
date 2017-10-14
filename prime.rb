7d766b3a65f78e3b796994ba0e2f8bfc9e3000b6
Kayla Hansen <shiverr@gmail.com>
2017-10-14 00:58:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(number)
   prime_number = number.to_a
-  if prime_number.to_a.any? {|i| number % i == 0 }
+  if prime_number.any? {|i| number % i == 0 }
     true
   else
     false
